7ae93348f257d965ee5e457fda6824e7ce0cc969
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-04-11 21:04:43 +0300

    gönderiler dizisi işlendi her döngüde bir Gönderi bileşeni çağırıldı

--- src/bilesenler/Gonderiler/Gonderiler.js
+++ src/bilesenler/Gonderiler/Gonderiler.js
@@ -8,6 +8,13 @@ const Gonderiler = (props) => {
 
   return (
     <div className="posts-container-wrapper">
+      {gonderiler.map((gonderi) => (
+        <Gonderi
+          key={gonderi.id}
+          gonderi={gonderi}
+          gonderiyiBegen={gonderiyiBegen}
+        />
+      ))}
       {/* gönderiler dizisini işleyip her döngüde bir Gönderi bileşeni çağırın*/}
       {/* Gönderi'nin çağırılmasında hangi propları kullanmanız gerektiğine dikkat edin! */}
     </div>
