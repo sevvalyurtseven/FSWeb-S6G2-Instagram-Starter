1ca5256dc050ae4c5e365a3e4c91a756b20b3799
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-04-11 22:10:26 +0300

    post 1 kere begenilebiliyor

--- src/bilesenler/Gonderiler/BegenBolumu.js
+++ src/bilesenler/Gonderiler/BegenBolumu.js
@@ -2,19 +2,27 @@
 // Doğru beğeni sayısını görüntülemek için proplardan gelen bir veri parçasını kullanın.
 // Beğeni sayısını artırmak için "gonderiyiBegen" fonksiyonunu kullanan bir onClick işleyicisi de ekleyeceksiniz.
 // (Ek görev olarak, kullanıcınızın aynı gönderiyi birden fazla kez "beğenmesini" engelleyin.)
-import React from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faComment, faHeart } from "@fortawesome/free-regular-svg-icons";
 
 const BegenBolumu = (props) => {
   // 🔥 Bu bileşenin parentının aşağıdaki propları düzgün gönderdiğinden emin olun.
   const { gonderiyiBegen, begeniSayisi } = props;
+  const [begeniliyorMu, setBegeniliyorMu] = useState(false);
+
+  const begeniHandler = () => {
+    if (begeniliyorMu === false) {
+      setBegeniliyorMu(true);
+      gonderiyiBegen();
+    }
+  };
 
   return (
     <div>
       <div className="like-section" key="likes-icons-container">
-        <div className="like-section-wrapper" onClick={gonderiyiBegen}>
-          <FontAwesomeIcon icon={faHeart} />
+        <div className="like-section-wrapper" onClick={begeniHandler}>
+          <FontAwesomeIcon icon={faHeart} color={begeniliyorMu ? "red" : ""} />
         </div>
         <div className="like-section-wrapper">
           <FontAwesomeIcon icon={faComment} />
